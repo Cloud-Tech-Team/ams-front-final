98a465f4aea2dfcc94143b7d3fa6063e440af39e
AmalS24 <amalmsp64@gmail.com>
2022-11-16 15:45:23 +0530

    'records'

--- src/Admin/Components/Records.js
+++ src/Admin/Components/Records.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Records = () => {
   return (
-    <div className='w-full bg-white h-12 rounded-md'>
-
+    <div className='w-full flex items-center p-2 justify-between bg-white  rounded-md'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0NlGVCtXmSW_seQQ8ksxj24ZnWiLPRxdPTevd8xxB0A&s' className='w-9 h-9 rounded-full' alt='avatar' />
+        <p>NBT230011</p>
+        <p>Nandakrishnan PN</p>
+        <p>CSE</p>
+        <p>BTech</p>
+        <p>8848978215</p>
+        <p className='text-green-500'>Payment completed</p>
+        <p className='underline'>print</p>
+        <p className='underline'>view more</p>
     </div>
   )
 }
--- src/Admin/Pages/Nri.js
+++ src/Admin/Pages/Nri.js
@@ -1,11 +1,18 @@
 
 import React from 'react'
 import Records from '../Components/Records'
-const cards = [0,1,2,3,4,5,7]
+const cards = [0,1,2,3,5,7]
 
 const Nri = () => {
   return (
     <div className="overflow-x-auto scrollbar-thin space-y-3 border-[1px] p-4 border-black scrollbar-thumb-rounded-full scrollbar-thumb-black ">
+        <div className='w-full flex space-x-4 items-end justify-end h-auto'>
+            <div className='w-auto h-8 rounded-full bg-white'>
+                {/* <input */}
+            </div>
+            <div className='w-9 h-9 rounded-full bg-white'></div>
+
+        </div>
         {cards.map((index)=>{
             return(<Records key={index} />)
           
