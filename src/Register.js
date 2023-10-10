18dd33a5069a5bd68b5a07eebcc9e4c53571c5f4
Aanand Deva Suresh <122597947+Keyes1@users.noreply.github.com>
2023-10-10 19:30:08 +0530

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -83,9 +83,9 @@ function Register() {
         <CallIcon fontSize="small" />
         <b className="text-red-600">Technical Support</b>
         <br />
-        Mr. Nandakrishnan: &nbsp;<b>88489 78215</b>
+        Ms. Jeena Varghese: &nbsp;<b>88489 78215</b>
         <br />
-        Mrs. Elby Baby: &nbsp;<b>96332 36639</b>
+        Ms. Rija Jose: &nbsp;<b>96332 36639</b>
         <br />
       </p>
       <Link to="/">
