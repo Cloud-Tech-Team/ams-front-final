aa4f18e85d2e53472f4971a1d4454530e52dbf01
Aanand Deva Suresh <122597947+Keyes1@users.noreply.github.com>
2023-10-10 19:45:07 +0530

    Update TitleBar.js

--- src/Admin/Components/TitleBar.js
+++ src/Admin/Components/TitleBar.js
@@ -45,6 +45,8 @@ const TitleBar = (props) => {
           <option value="2020">2020</option>
           <option value="2021">2021</option>
           <option value="2022">2022</option>
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
         </select>
         <img
           src="https://cdn-icons-png.flaticon.com/512/2645/2645897.png"
